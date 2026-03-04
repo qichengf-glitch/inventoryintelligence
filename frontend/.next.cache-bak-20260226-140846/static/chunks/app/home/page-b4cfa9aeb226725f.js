8bd8bb7cfcaf47086bef2fe57761dada75392180
傅启诚 <qichengfu@Qicheng-Fus-MBP.lan>
2026-03-04 10:28:47 -0500

    fix(deploy): remove tracked build caches and tighten ignore rules

--- frontend/.next.cache-bak-20260226-140846/static/chunks/app/home/page-b4cfa9aeb226725f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[951],{2233:function(n,e,u){Promise.resolve().then(u.bind(u,7996))}},function(n){n.O(0,[996,971,117,744],function(){return n(n.s=2233)}),_N_E=n.O()}]);
